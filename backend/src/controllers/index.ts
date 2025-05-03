d5dc57d2678263a2b71b7f520c539c375f363320
Girish-Gaurav-Sharma <21bph009@nith.ac.in>
2025-05-03 12:27:55 +0530

    Remove backend and frontend codebase including configurations, routes, controllers, and components

--- backend/src/controllers/index.ts
+++
@@ -1,9 +0,0 @@
-export class IndexController {
-    public getHello(req, res) {
-        res.send('Hello World');
-    }
-
-    public getGoodbye(req, res) {
-        res.send('Goodbye World');
-    }
-}
